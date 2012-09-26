260057097adf331241d0c234991ed32c96090fab
unknown <Rahul@Rahul-Dell.(none)>
2012-09-27 00:33:59 +0530

    display del btn when user clicked on edit btn

--- efridge/src/com/efridge/activity/MyFridgeActivity.java
+++ efridge/src/com/efridge/activity/MyFridgeActivity.java
@@ -6,43 +6,70 @@ package com.efridge.activity;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.efridge.listener.MyFridgeClickListener;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class MyFridgeActivity extends BaseActivity {
 
+	boolean isDelVisible = false;
+	FoodListAdapter foodListAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) { // TODO
-		
-		
+
 		setContentView(R.layout.myfridge_layout);
 		super.onCreate(savedInstanceState);
-		
-		Button addFoodBtn = (Button)findViewById(R.id.addFoodBtn);
-		MyFridgeClickListener myFridgeClickListener = new MyFridgeClickListener();
-		addFoodBtn.setOnClickListener(myFridgeClickListener);
-		
-        List<String> foods = new ArrayList<String>();
-        
-        for(int i = 0; i < 10; i++){
-        	foods.add("Chicken");
-        }
-        
-        ListView foodListView = (ListView)findViewById(R.id.foodsList);
-        FoodListAdapter foodListAdapter = new FoodListAdapter(foods);
-        foodListView.setAdapter(foodListAdapter);
+
+		Button addFoodBtn = (Button) findViewById(R.id.addFoodBtn);
+		Button editFoodBtn = (Button) findViewById(R.id.editFoodBtn);
+
+		addFoodBtn.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+
+				Intent intent = new Intent(v.getContext(), AddFoodActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+				v.getContext().startActivity(intent);
+			}
+		});
+
+		editFoodBtn.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+
+				ImageButton delBtn = (ImageButton) findViewById(R.id.delBtn);
+				isDelVisible = !isDelVisible;
+
+				System.out.println("delBtn : " + delBtn);
+				if (isDelVisible)
+					delBtn.setVisibility(View.VISIBLE);
+				else
+					delBtn.setVisibility(View.INVISIBLE);
+				
+				foodListAdapter.notifyDataSetChanged();
+			}
+		});
+
+		List<String> foods = new ArrayList<String>();
+
+		for (int i = 0; i < 10; i++) {
+			foods.add("Chicken");
+		}
+
+		ListView foodListView = (ListView) findViewById(R.id.foodsList);
+		foodListAdapter = new FoodListAdapter(foods);
+		foodListView.setAdapter(foodListAdapter);
 	}
 
-
-	
 	class FoodListAdapter extends BaseAdapter {
 
 		List<String> foodList;
@@ -50,7 +77,7 @@ public class MyFridgeActivity extends BaseActivity {
 		FoodListAdapter(List<String> foodList) {
 			this.foodList = foodList;
 		}
-		
+
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
@@ -71,13 +98,20 @@ public class MyFridgeActivity extends BaseActivity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			
+
 			LayoutInflater inflater = getLayoutInflater();
 			View row = inflater.inflate(R.layout.foodlist_layout, parent, false);
-			
+
 			TextView foodView = (TextView) row.findViewById(R.id.food);
 			foodView.setText(this.foodList.get(position));
-			
+
+			ImageButton delButton = (ImageButton) row.findViewById(R.id.delBtn);
+			if (isDelVisible) {
+
+				delButton.setVisibility(View.VISIBLE);
+			} else {
+				delButton.setVisibility(View.INVISIBLE);
+			}
 			return row;
 		}
 	}
--- efridge/src/com/efridge/listener/MyFridgeClickListener.java
+++ efridge/src/com/efridge/listener/MyFridgeClickListener.java
@@ -2,23 +2,50 @@
 // Just created this activity
 package com.efridge.listener;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 import com.efridge.activity.AddFoodActivity;
 import com.efridge.activity.R;
 
-public class MyFridgeClickListener implements OnClickListener {
+public class MyFridgeClickListener extends Activity implements OnClickListener {
 
+	boolean isDelVisible = false;
+/*	ImageButton delBtn;
+	
+	public ImageButton getDelBtn() {
+		return delBtn;
+	}
+
+	public void setDelBtn(ImageButton delBtn) {
+		this.delBtn = delBtn;
+	}
+*/
 	@Override
 	public void onClick(View v) {
+		
 		
 		if (v.getId() == R.id.addFoodBtn) {
 			
 			Intent intent = new Intent(v.getContext(), AddFoodActivity.class);
 			intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 			v.getContext().startActivity(intent);
-		}
+			
+		} /*else if(v.getId() == R.id.editFoodBtn){
+
+			System.out.println("---------------");
+			
+			ImageButton delBtn = (ImageButton)findViewById(R.id.delBtn);
+			isDelVisible = !isDelVisible;
+			
+			System.out.println("delBtn : " + delBtn);
+			if(isDelVisible)
+				delBtn.setVisibility(View.VISIBLE);
+			else 
+				delBtn.setVisibility(View.INVISIBLE);
+		}*/
 	}
 }
